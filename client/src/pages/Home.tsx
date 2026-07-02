ea875c49d2feb67b49205ee73700b0385f752a19
garvin <garvinromelus@outlook.com>
2026-07-02 11:45:45 -0400

    basic react hook form created with even more basic input validation

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,7 +5,15 @@ import {
   IonTitle, 
   IonToolbar,
   IonInput,
-  IonButton } from '@ionic/react';
+  IonButton 
+} from '@ionic/react';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 import { useForm } from "react-hook-form";
 //import GameForm from './components/GameForm';
@@ -18,6 +26,14 @@ import { useForm } from "react-hook-form";
 // }
 
 const Home: React.FC = () => {
+    const mutation = useMutation({
+    mutationFn: (newTodo) => {
+      return fetch("/games")
+      //return axios.post('/todos', newTodo)
+    },
+  })
+
+
   const { register, handleSubmit,reset, formState:{errors} } = useForm({
     defaultValues:{
       roomCode:'1',
@@ -45,20 +61,25 @@ const Home: React.FC = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <IonInput 
           label="Room Code" 
-          {...register("roomCode", {required: "Please enter a room c"})}
+          {...register("roomCode", {required: "Please enter a room code."})}
           type='number'
           fill='outline'
           />
+          <p>{errors.roomCode?.message}</p>
+
           <IonInput 
           label="User"
-          {...register("userName", {required: true})}
+          {...register("userName", {required: "Enter a user name."})}
           fill='outline'
           />
+          <p>{errors.userName?.message}</p>
+
           <IonInput 
           label="Celebrity Name" 
-          {...register("celebName", {required: true})}
+          {...register("celebName", {required: "Please enter celebrity"})}
           fill='outline'
           />
+          <p>{errors.celebName?.message}</p>
           <IonButton type="submit">Submit!</IonButton>
         </form>
       </IonContent>
