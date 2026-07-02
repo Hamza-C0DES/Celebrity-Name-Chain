80494d9091affdcd2788448c4048558d66f1137b
garvin <garvinromelus@outlook.com>
2026-07-02 13:00:12 -0400

    added cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 const app = express();
+app.use(cors);
 
 import { PrismaClient } from "@prisma/client/extension";
 
