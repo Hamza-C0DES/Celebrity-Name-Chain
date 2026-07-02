020ccd843ad0c0a7f033a0061eedaf2cb14d173b
garvin <garvinromelus@outlook.com>
2026-07-02 13:56:32 -0400

    fixed client files

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,71 +1,33 @@
-import { IonButton, IonInput } from '@ionic/react';
-import { useForm } from "react-hook-form";
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar
+} from '@ionic/react';
 import {
-  useMutation,
-  useQueryClient
+  QueryClient,
+  QueryClientProvider,
 } from '@tanstack/react-query'
+import GameForm from '../components/GameForm';
 
-const GameForm =()=>{
-    const queryClient = useQueryClient();
-  
-  const { register, handleSubmit,reset, formState:{errors} } = useForm({
-    defaultValues:{
-      roomCode:'1',
-      userName: "johndoe",
-      celebName: "johndoe"
-    }
-    });
+const queryClient = new QueryClient()
 
-    const onSubmit = (data: any) =>{
-        const res = mutation.mutate(data);
-        console.log(res);
-        return res;
-    };
+const Home: React.FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Celebrity Name Chain</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <GameForm/>
+      </IonContent>
+    </IonPage>
+    </QueryClientProvider>
+  );
+};
 
-    const mutation = useMutation({
-    mutationFn: async (data)=>{
-        return await fetch("http://localhost:3000/game",{
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-        }
-        
-        );
-    },
-
-    })
-    
-
-    // const [userName, setName] = useState('');
-    // const [roomCode, setRoom] = useState('');
-    return<>
-    <form onSubmit={handleSubmit(onSubmit)}>
-          <IonInput 
-          label="Room Code:" 
-          {...register("roomCode", {required: "Please enter a room code."})}
-          type='number'
-          fill='outline'
-          />
-          <p>{errors.roomCode?.message}</p>
-
-          <IonInput 
-          label="User:"
-          {...register("userName", {required: "Enter a user name."})}
-          fill='outline'
-          />
-          <p>{errors.userName?.message}</p>
-
-          <IonInput 
-          label="Celebrity Name:" 
-          {...register("celebName", {required: "Please enter celebrity"})}
-          fill='outline'
-          />
-          <p>{errors.celebName?.message}</p>
-          <IonButton type="submit">Submit!</IonButton>
-        </form>
-    </>;
-}
-
-export default GameForm;
+export default Home;
